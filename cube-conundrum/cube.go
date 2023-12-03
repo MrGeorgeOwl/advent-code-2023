169a9312da4ad9ea5556a855011bf1fb2d07f3ef
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2023-12-03 19:29:09 +0100

    solve cube conundrum question

---
+++ cube-conundrum/cube.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type game struct {
+	Id       int
+	MaxRed   int
+	MaxGreen int
+	MaxBlue  int
+}
+
+func (g game) String() string {
+	return fmt.Sprintf("{id: %v, red: %v, green: %v, blue: %v}", g.Id, g.MaxRed, g.MaxGreen, g.MaxBlue)
+}
+
+var cube = regexp.MustCompile(`(\d+)\s+(blue|red|green)`)
+
+func main() {
+	games, err := parseGamesFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filtered := filterGamesWithUpperBound(games, 12, 13, 14)
+
+	var sum int
+	for _, item := range filtered {
+		sum += item.Id
+	}
+
+	fmt.Println(sum)
+}
+
+func parseGamesFile() ([]game, error) {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		return nil, fmt.Errorf("parseGamesFile: reading file: %s", err)
+	}
+	defer f.Close()
+
+	games := make([]game, 0)
+	scanner := bufio.NewScanner(f)
+	counter := 1
+	for scanner.Scan() {
+		g := game{Id: counter}
+		line := scanner.Text()
+		err := parseLine(&g, line)
+		if err != nil {
+			log.Fatalf("parseGamesFile: %s\n", err)
+			continue
+		}
+		counter++
+		games = append(games, g)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("parseGamesFile: scanning file: %s", err)
+	}
+
+	return games, nil
+}
+
+func parseLine(g *game, line string) error {
+	for _, v := range cube.FindAllString(strings.Split(line, ":")[1], -1) {
+		splitted := strings.Split(v, " ")
+		amount, err := strconv.Atoi(splitted[0])
+		if err != nil {
+			return fmt.Errorf("parseLine: %s, Subset: %s", err, v)
+		}
+
+		switch splitted[1] {
+		case "red":
+			if amount > g.MaxRed {
+				g.MaxRed = amount
+			}
+		case "blue":
+			if amount > g.MaxBlue {
+				g.MaxBlue = amount
+			}
+		case "green":
+			if amount > g.MaxGreen {
+				g.MaxGreen = amount
+			}
+		}
+	}
+
+	return nil
+}
+
+func filterGamesWithUpperBound(games []game, r, g, b int) []game {
+	filter := make([]game, 0)
+	for _, item := range games {
+		if item.MaxRed <= r && item.MaxGreen <= g && item.MaxBlue <= b {
+			filter = append(filter, item)
+		}
+	}
+
+	return filter
+}
---
+++ cube-conundrum/go.mod
@@ -0,0 +1,3 @@
+module github.com/MrGeorgeOwl/adven/cube
+
+go 1.21.1
